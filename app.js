9c11be9137d605398fb6d1b2f9bacb6efb1ff1ba
subhashis2204 <subhashispaul2204@gmail.com>
2021-12-30 11:58:08 +0530

    added typing animation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const burger = document.querySelector('.burger')
 const nav = document.querySelector('.nav-links')
 const navlinks = document.querySelectorAll('.nav-links li')
+const typewriters = document.querySelectorAll('.typewriter')
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('nav-active')
@@ -14,6 +15,44 @@ burger.addEventListener('click', () => {
             link.style.animation = `navlinkfade 0.5s ease forwards ${index / 5 + 1}s`
         }
     })
-
     burger.classList.toggle('toggle')
 })
+
+// const lines = ['Hello, I am Subhashis.','I am a web developer','I am a quantum computing enthusiast','I am a competitive coder']
+
+const line = 'Hello, I am Subhashis.'
+let timer
+
+function typingeffect() {
+    let characters = line.split("")
+    let looptyping = function () {
+        if (characters.length > 0) {
+            document.querySelector('.typewriter').innerHTML += characters.shift()
+        }
+        else {
+            deletingeffect();
+            return false;
+        }
+        timer = setTimeout(looptyping, 400)
+    }
+    looptyping();
+}
+
+function deletingeffect() {
+    let characters = line.split('')
+    let loopdeleting = function () {
+        if (characters.length > 0) {
+            characters.pop()
+            document.querySelector('.typewriter').innerHTML = characters.join('')
+        }
+        else {
+            typingeffect()
+            return false
+        }
+        timer = setTimeout(loopdeleting, 150);
+    }
+    loopdeleting();
+}
+
+typingeffect()
+
